564575cfbcf889fc21a07bafd21260e42cb25927
duygukucukoglu35 <duygukucukoglu35@gmail.com>
2022-12-16 16:34:56 +0300

    xyz

--- src/App.js
+++ src/App.js
@@ -1,59 +1,49 @@
 import { React, useState } from "react";
+ function App() {
+   const [counter, setCounter] = useState(0);
 
-function App() {
-  const [counter, setCounter] = useState(0);
-
-  //increase counter
+ //increase counter fonksiyonum
   const increase = () => {
     setCounter((count) => count + 1);
   };
 
-  //decrease counter
-  const decrease = () => {
-    setCounter((count) => (count - 1) > 0);
-  };
+//decrease counter fonksiyonum
+ const decrease = () => {
+ setCounter((count) => (count - 1) > 0);
+ };
 
-  //reset counter
-  const reset = () => {
-    setCounter(0);
-  };
+  //reset counter fonksiyonum
+ const reset = () => {
+setCounter(0);
+ };
 
-
-// const handleClick=event=>{
-
-// }
-
-  return (
+ return (
     <div className="container">
-      <div className="counter">
-       
-        <div className="btn__container">
-        <span className="counter__output">{counter}</span>
-          
+       <div className="counter">
+       <div className="btn__container">
+       <span className="counter__output">{counter}</span>
           <div className="all">
-          <div className="btn">
-            <button className="control__btn1" onClick={increase}>
-              +
-            </button>
-            <button className="control__btn2" onClick={decrease}>
+         <div className="btn">
+            <button className="control__btn1" onClick={increase}> {/*butona tıklayınca artıyor*/}
+              +             </button>
+             <button className="control__btn2" onClick={decrease}>  {/*butona tıklayınca azalıyor*/}
               -
             </button>
           </div>
           <div className="rst">
-            <button className="reset" onClick={reset}>
+             <button className="reset" onClick={reset}>   {/*butona tıklayınca sıfırlıyor*/}
               Reset
             </button>
+           </div>
           </div>
-          </div>
-        </div>
-      </div>
+         </div>
+       </div>
       <div className="counter__enter">
-        <span className="text">
-          {(counter % 10 ==0) ? <p>{counter} Sayısı gösterilmektedir</p> : <p></p>}
-          </span>
-      </div>
-    </div>
-  );
-}
-
-export default App;
+         <span className="text">
+          {(counter % 10 ==0) ? <p>{counter} Sayısı gösterilmektedir</p> : <p></p>} {/*10,20,30 olduğunda yazım görünmeli değilse görünmesin*/}
+           </span>
+       </div>
+     </div>
+   );
+ }
+ export default App;
